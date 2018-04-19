36a170a60404edda192e8907f323ee51a5f9028f
bree melechinsky <groundcontroltomajorbree@gmail.com>
2018-04-19 11:28:02 -0500

    add records route and collection list to DOM

--- server/public/scripts/script.js
+++ server/public/scripts/script.js
@@ -2,6 +2,17 @@ console.log('js czech');
 
 $(document).ready(onReady);
 
-function onReady()  {
+function onReady() {
     console.log('jquery czech');
+
+    $.ajax({
+        type: 'GET',
+        url: '/records'
+    })
+        .then(function (response) {
+            for (let i = 0; i < response.length; i++) {
+                console.log(response[i]);
+                $('#recordList').append(`<li> ${response[i].title} by ${response[i].artist}, ${response[i].year} | $${response[i].cost} </li>`);
+            }
+        });
 }
--- server/server.js
+++ server/server.js
@@ -2,7 +2,17 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 
+//this will live in the database shortly
+const recordCollection = require('./modules/record-collection.js');
+
 app.use(express.static('server/public'));
+
+//routes (get, post, put delete reqs)
+
+app.get('/records', (req, res) => {
+    res.send(recordCollection);
+});
+
 
 app.listen(PORT, () =>{
     console.log(`up and running on ${PORT}`);
